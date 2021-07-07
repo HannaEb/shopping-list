b06bef4eb9fb1f63aca91b7fb57d5318e91f925e
Hanna <hannaeberhoefer@yahoo.com>
2021-07-07 15:47:30 +0100

    Create ListItemForm

---
+++ src/components/ListItemForm/ListItemForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class ListItemForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <label>Quantity</label>
+                        <input type='number' />
+                    </div>
+                    <div>
+                        <label>Unit</label>
+                        <select>
+                            <option>Item</option>
+                            <option>Kilograms</option>
+                            <option>Grams</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Product</label>
+                        <input type='text' />
+                    </div>
+                    <div>
+                        <label>Catergory</label>
+                        <select>
+                            <option>Fresh</option>
+                            <option>Bakery</option>
+                            <option>Cupboard</option>
+                            <option>Frozen</option>
+                            <option>Chilled</option>
+                            <option>Drinks</option>
+                            <option>Household</option>
+                            <option>Other</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ListItemForm;
