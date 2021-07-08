c82535fd5f0386e694451f58b3df3755baf09c7e
Hanna <hannaeberhoefer@yahoo.com>
2021-07-08 10:32:55 +0100

    Open form functionality

--- src/components/ToggleableListItemForm/ToggleableListItemForm.js
+++ src/components/ToggleableListItemForm/ToggleableListItemForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import ListItemForm from '../ListItemForm/ListItemForm';
 
 class ToggleableListItemForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+        };
+
+        this.handleFormOpen = this.handleFormOpen.bind(this);
+    }
+
+    handleFormOpen() {
+        this.setState({ isOpen: true });
+    }
+
     render() {
-        return (
-            <button>
-                Add
-            </button>
-        )
+        if (this.state.isOpen) {
+            return (
+                <ListItemForm />
+            )
+        } else {
+            return (
+                <button onClick={this.handleFormOpen}>
+                    Add
+                </button>
+             )
+        }
     }
 }
 
